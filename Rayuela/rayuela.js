de34aa51924d80b43a0a6853ba190da4678a543f
Funmi <oluwafunmi.ojo@gmail.com>
2016-01-22 16:57:04 -0500

    Update rayuela.js

--- Rayuela/rayuela.js
+++ Rayuela/rayuela.js
@@ -16,7 +16,7 @@ window.onload = function(){
 	}};
 	request.overrideMimeType("application/json");
 	request.open("GET", 
-	"https://github.com/FunmiOjo/rayuela-quote-machine/rayuelaQuotes.json");
+	"https://github.com/FunmiOjo/funmiojo.github.io/Rayuela/rayuela.json");
 	request.setRequestHeader('Accept', 'application/json')
 	request.send(null);	
 }
