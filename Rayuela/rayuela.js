618cbe79b622cffd88a6fa8dbfbb83b115baacd9
Funmi <oluwafunmi.ojo@gmail.com>
2016-01-28 12:48:41 -0500

    Add Twitter button behavior
    Add statement to readystate function that adds text of quote to tweet composition window
    Add code from Twitter API

--- Rayuela/rayuela.js
+++ Rayuela/rayuela.js
@@ -1,15 +1,19 @@
-function displayRandomQuote(parsedData) {	
+function readyTasks(parsedData) {	
 	var randomQuoteIndex = Math.floor(Object.keys(parsedData).length * Math.random());
-	document.getElementById("quoteDisplay").innerHTML = "\"" + parsedData[randomQuoteIndex].esQuote + "\"";
+	var randomQuote = parsedData[randomQuoteIndex].esQuote;
+	var withoutSpaces = randomQuote.split(" ").join("%20")
+	var tweetText = "https://twitter.com/intent/tweet?text=" + randomQuote.split(" ").join("%20");
+	document.getElementById("quoteDisplay").innerHTML = "\"" + randomQuote + "\"";
+	document.getElementById("tweetbutton").setAttribute("href", tweetText);
 }
-
+	
 window.onload = function(){
 	//Get quote data after button is clicked
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function() {
 	if (request.readyState == 4 && request.status == 200) {
 		var parsedData = JSON.parse(request.responseText).quotes;
-		displayRandomQuote(parsedData);		
+		readyTasks(parsedData);	
 	}};
 
 	request.open("GET", 
@@ -23,10 +27,27 @@ document.getElementById("quoteButton").onclick = function(){
 	request.onreadystatechange = function() {
 	if (request.readyState == 4 && request.status == 200) {
 		var parsedData = JSON.parse(request.responseText).quotes;
-		displayRandomQuote(parsedData);		
+		readyTasks(parsedData);		
 	}};
 	
 	request.open("GET", 
 	"https://crossorigin.me/http://funmiojo.com/Rayuela/rayuela.json", true);
 	request.send(null);	
 };
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
